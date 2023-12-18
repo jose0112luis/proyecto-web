604dc71750327c65c64f5656fe67d60ebab55272
jose0112luis <jose0112luis@hotmail.com>
2023-12-18 11:27:27 -0500

    Index actualizado para llenar los campos del excel.

--- src/main/java/org/proyecto/managedbeans/ProyectoMB.java
+++ src/main/java/org/proyecto/managedbeans/ProyectoMB.java
@@ -11,6 +11,9 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import org.proyecto.config.Constantes;
+import org.proyecto.model.Area;
+import org.proyecto.model.Persona;
+import org.proyecto.model.Proceso;
 import org.proyecto.model.Proyecto;
 import org.proyecto.services.interfaces.AppServices;
 
@@ -25,20 +28,50 @@ public class ProyectoMB implements Serializable {
     @Inject
     private AppServices appService;
     
-    private List<Proyecto> proyectos;
+    private List<Area> areas;
+    private List<Proceso> procesos;
+    private List<Persona> personas;
+    
+    private Proyecto proyecto;
     
     @PostConstruct
     public void init() {
-        proyectos = appService.methodListGET(Constantes.appUrl+ "consultarProyecto", Proyecto[].class);
+        proyecto = new Proyecto();
+        areas = appService.methodListGET(Constantes.appUrl+ "consultarArea", Area[].class);
+        procesos = appService.methodListGET(Constantes.appUrl+ "consultarProceso", Proceso[].class);
+        personas = appService.methodListGET(Constantes.appUrl+ "consultarPersona", Persona[].class);
     }
 
-    public List<Proyecto> getProyectos() {
-        return proyectos;
+    public List<Area> getAreas() {
+        return areas;
     }
 
-    public void setProyectos(List<Proyecto> proyectos) {
-        this.proyectos = proyectos;
+    public void setAreas(List<Area> areas) {
+        this.areas = areas;
+    }
+
+    public List<Proceso> getProcesos() {
+        return procesos;
+    }
+
+    public void setProcesos(List<Proceso> procesos) {
+        this.procesos = procesos;
     }
     
-    
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(List<Persona> personas) {
+        this.personas = personas;
+    }
+
+    public Proyecto getProyecto() {
+        return proyecto;
+    }
+
+    public void setProyecto(Proyecto proyecto) {
+        this.proyecto = proyecto;
+    }
+
 }
